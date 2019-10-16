06135713faa5cf7393afca766069e3421f93efcb
VladKivich <vladkivich@gmail.com>
2019-10-16 19:23:14 +0300

    ProgressBar

---
+++ Assets/Scripts/ProgressBarScript/ProgressBarScript.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProgressBar
+{
+    public class ProgressBarScript : MonoBehaviour
+    {
+        [SerializeField] private Text clickCount;
+        [SerializeField] private Text reduceTime;
+        [SerializeField] private Button increaseTime;
+        [SerializeField] private Button decreaseTime;
+        [SerializeField] private Image Fill;
+
+        private Coroutine reduceTimeCoroutine;
+        private float clickCountReduseTime;
+        private const float MAX_REDUCE_TIME = 5f;
+        private const float MIN_REDUCE_TIME = 0.25f;
+        private int countOfClicks;
+
+        private void Awake()
+        {
+            reduceTimeCoroutine = StartCoroutine("ReduceBrogressBar");
+            clickCountReduseTime = 1f;
+            increaseTime.onClick.AddListener(IncreaseTime);
+            decreaseTime.onClick.AddListener(DecreaseTime);
+        }
+
+        private IEnumerator ReduceBrogressBar()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(clickCountReduseTime);
+                var amount = Fill.fillAmount;
+                if (amount > 0)
+                {
+                    amount -= 0.01f;
+                    Fill.fillAmount = amount;
+                }
+            }
+        }
+
+        private void IncreaseTime()
+        {
+            if (clickCountReduseTime < MAX_REDUCE_TIME)
+            {
+                clickCountReduseTime += 0.25f;
+                ChangeTime();
+            }
+        }
+
+        private void DecreaseTime()
+        {
+            if (clickCountReduseTime > MIN_REDUCE_TIME)
+            {
+                clickCountReduseTime -= 0.25f;
+                ChangeTime();
+            }
+        }
+
+        private void ChangeTime()
+        {
+            reduceTime.text = String.Format($"{clickCountReduseTime.ToString()} sec");
+        }
+
+        public void IncreaseClickCount()
+        {
+            if (Fill.fillAmount < 1f)
+            {
+                Fill.fillAmount += 0.01f;
+                countOfClicks++;
+                ChangeClickCount();
+            }
+        }
+
+        private void ChangeClickCount()
+        {
+            clickCount.text = countOfClicks.ToString();
+        }
+
+        public void ResetClickCount()
+        {
+            countOfClicks = 0;
+            ChangeClickCount();
+        }
+    }
+}
