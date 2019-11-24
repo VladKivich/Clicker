c723a2292a7c24a16f0cd0b87f6b5c4d74d79373
OneRims <OneRims@users.noreply.github.com>
2019-11-24 22:55:10 +0500

    Time v 1.0

--- Assets/Scripts/Services/TimeService.cs
+++ Assets/Scripts/Services/TimeService.cs
@@ -1,8 +1,57 @@
-﻿namespace Service
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Service
 {
-    public class TimeMa
-    {
 
+
+    public class TimeService : MonoBehaviour
+    {
+        private int gameHours { get; set; }
+        private int gameMinutes { get; set; }
+        private int gameSeconds { get; set; }
+
+        private int zero { get; set; }
+        private string zeroText;
+
+        string stringMinutes;
+        string stringHours;
+
+        public Text TextTime;
+
+        void Update()
+        {
+            gameSeconds += 1;
+            if (gameSeconds == 3)
+            {
+                gameSeconds -= 3;
+                gameMinutes++;
+
+                if (gameMinutes == 60)
+                {
+                    gameMinutes -= 60;
+                    gameHours++;
+
+                    if (gameHours == 24)
+                    {
+                        gameHours -= 24;
+                        Debug.Log("День прошёл");
+                    }
+                }
+            }
+
+            if (gameHours <= 23 && gameHours >= 10) zero = 1;
+            else zero = 2;
+
+            if (zero == 1) zeroText = "";
+            else if (zero == 2) zeroText = "0";
+
+            stringMinutes = gameMinutes.ToString();
+            stringHours = gameHours.ToString();
+
+            TextTime.text = "Время " + zeroText + stringHours + ":00 ";
+        }
     }
+
 }
 
